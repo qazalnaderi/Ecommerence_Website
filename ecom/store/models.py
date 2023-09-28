cc94dec9d6beb866e5704d0b94ad3d11899df643
qazalnaderi <ghazalnaderi4@gmail.com>
2023-09-28 13:18:56 +0330

    Order class created

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -25,3 +25,14 @@ class Product(models.Model):
     image = models.ImageField(upload_to='uploads/product/')
     def __str__(self):
         return self.name
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE )
+    costumer = models.ForeignKey(Costumer, on_delete=models.CASCADE )
+    quantity = models.ImageField(default=1)
+    address = models.CharField(max_length=100,default='',blank=True)
+    phone = models.CharField(max_length=20,default='',blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+    def __str__(self):
+        return self.product
+
