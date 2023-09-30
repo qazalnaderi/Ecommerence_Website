f793eddcfd2c414e440728865c8613ae41442a0e
qazalnaderi <ghazalnaderi4@gmail.com>
2023-09-30 16:16:26 +0330

    Adding a built-in template to project

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Costumer, Category, Product, Order
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Costumer)
+admin.site.register(Product)
+admin.site.register(Order)
--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,38 +1,45 @@
 from django.db import models
 import datetime
 
-#Category of products
+
+# Category of products
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+
 class Costumer(models.Model):
-    fname =models.CharField(max_length=50)
-    lname =models.CharField(max_length=50)
-    phone =models.IntegerField(max_length=12)
-    email =models.EmailField(max_length=20)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    email = models.EmailField(max_length=20)
     password = models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.fname} {self.lname}'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE , default=1)
-    description = models.CharField(max_length=200,default='',blank=True,null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=200, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+
     def __str__(self):
         return self.name
+
+
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE )
-    costumer = models.ForeignKey(Costumer, on_delete=models.CASCADE )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    costumer = models.ForeignKey(Costumer, on_delete=models.CASCADE)
     quantity = models.ImageField(default=1)
-    address = models.CharField(max_length=100,default='',blank=True)
-    phone = models.CharField(max_length=20,default='',blank=True)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanField(default=False)
+
     def __str__(self):
         return self.product
-
