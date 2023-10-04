4d1e7e76d2c4baf8fd9edaeadc5020215cc90552
qazalnaderi <ghazalnaderi4@gmail.com>
2023-10-04 22:52:39 +0330

    about page created

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -4,4 +4,8 @@ from .models import Product
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products':products})
+    return render(request, 'home.html', {'products': products})
+
+
+def about(request):
+    return render(request, 'about.html', {})
