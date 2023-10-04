34dc40ee529ef45ad8428d163526ac40d05ec7b8
qazalnaderi <ghazalnaderi4@gmail.com>
2023-10-04 14:58:39 +0330

    adding sale option

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -30,6 +30,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=200, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
 
     objects = models.Manager()
 
