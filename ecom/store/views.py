e2546961fe51e22e84f2c20a6574332039e6e37b
qazalnaderi <ghazalnaderi4@gmail.com>
2023-10-05 14:58:32 +0330

    login added

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -9,3 +10,12 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    pass
+
